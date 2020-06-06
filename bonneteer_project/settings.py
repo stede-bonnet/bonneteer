9be3e45b3ac4b80e729f560b1602e6c5742f8155
kungFuLambda <leon.tadina@gmail.com>
2020-06-06 11:53:41 +0100

    req and setting

--- bonneteer_project/settings.py
+++ bonneteer_project/settings.py
@@ -31,7 +31,8 @@ MEDIA_URL = '/media/'
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'nul')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG','False')
+
 
 ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
