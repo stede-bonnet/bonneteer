6d799f62c58685cde8d0deb42a1105d7c0958bef
kungFuLambda <leon.tadina@gmail.com>
2020-06-06 12:02:48 +0100

    dbShanenagins

--- bonneteer_project/settings.py
+++ bonneteer_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates') ##.join will join the base file location and whatever you add in proper format
@@ -87,6 +88,7 @@ WSGI_APPLICATION = 'bonneteer_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -136,4 +138,5 @@ STATICFILES_DIRS = [STATIC_DIR,]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+django_heroku.settings(locals())
