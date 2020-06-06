3f430df929461fabed8dd5258200ee23a0f3d3c5
kungFuLambda <leon.tadina@gmail.com>
2020-06-06 12:09:38 +0100

    removed obsolete reqs

--- bonneteer_project/settings.py
+++ bonneteer_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates') ##.join will join the base file location and whatever you add in proper format
@@ -139,4 +139,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-django_heroku.settings(locals())
