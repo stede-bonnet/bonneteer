71725d99a10ea276472fc900ee6ea542778b520f
kungFuLambda <leon.tadina@gmail.com>
2020-06-06 11:57:54 +0100

    modified:   bonneteer_project/settings.py

--- bonneteer_project/settings.py
+++ bonneteer_project/settings.py
@@ -136,4 +136,4 @@ STATICFILES_DIRS = [STATIC_DIR,]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
